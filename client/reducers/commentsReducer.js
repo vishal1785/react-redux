9ddff655e23cd4000b51883777290c8a64b5738c
Vishal Bhardwaj <Vishal.Bhardwaj@synechron.com>
2018-02-05 13:37:56 +0530

    modified posts data + changed local variable name + added gitignore

--- client/reducers/commentsReducer.js
+++ client/reducers/commentsReducer.js
@@ -1,18 +1,18 @@
-export default function comments(defaultStore=[],action) {
+export default function comments(defaultState=[],action) {
     switch(action.type){
 
         case 'FETCH_COMMENTS':
             console.log('withing FETCH_COMMENTS action');
-            defaultStore = action.response.data;
-            console.log(defaultStore);
-        return defaultStore;
+            defaultState = action.response.data;
+            console.log(defaultState);
+        return defaultState;
         
             case 'ADD_COMMENT':
             console.log(action);
             console.log('within comments reducer');
-        return defaultStore;
+        return defaultState;
         
             default:
-        return defaultStore;
+        return defaultState;
     }
 }
--- client/reducers/postsReducer.js
+++ client/reducers/postsReducer.js
@@ -1,22 +1,22 @@
-export default function posts(defaultStore=[],action) {
+export default function posts(defaultState=[],action) {
     
     switch(action.type){
         
         case 'FETCH_POSTS':
             console.log('withing FETCH_POSTS action');
-            defaultStore = action.response.data;
-            console.log(defaultStore);
-        return defaultStore;
+            defaultState = action.response.data;
+            console.log(defaultState);
+        return defaultState;
 
         case 'INCREMENT_LIKES':
             let postIndex = action.index;
             return [
-                ...defaultStore.slice(0,postIndex), //  slice the array and spread till index element
-                {...defaultStore[postIndex],likes:defaultStore[postIndex].likes+1}, //  get the object at index and spread it and set the likes to +1
-                ...defaultStore.slice(postIndex+1) // let the rest of the array be same
+                ...defaultState.slice(0,postIndex), //  slice the array and spread till index element
+                {...defaultState[postIndex],likes:defaultState[postIndex].likes+1}, //  get the object at index and spread it and set the likes to +1
+                ...defaultState.slice(postIndex+1) // let the rest of the array be same
             ];
 
         default:
-        return defaultStore;
+        return defaultState;
     }
 }
